7fbaf1391058c47f5ca062ce37721f9be705c8d6
badexp <i.ushakov98@gmail.com>
2019-04-10 12:44:54 +0200

    Implemented Service for work with http://www.icndb.com/api/
    Also added GuzzleHttp as dependency

--- src/Service/ICNDBService.php
+++ src/Service/ICNDBService.php
@@ -6,19 +6,53 @@ namespace App\Service;
 
 use App\Contract\Service\JokeServiceInterface;
 use App\Model\Joke;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Uri;
 
+/**
+ * Class ICNDBService
+ * Simple implementation for http://www.icndb.com/api/
+ * @package App\Service
+ */
 class ICNDBService implements JokeServiceInterface
 {
+    private const BASE_URL = 'api.icndb.com';
 
+    private const METHOD_RANDOM = '/jokes/random';
+    private const METHOD_CATEGORIES = '/categories';
+
+    /**
+     * Gets random Joke
+     * @param string|null $category
+     * @return Joke
+     */
     public function getRandom(string $category = null) : Joke
     {
-        // TODO: Implement getRandom() method.
-        return new Joke('Joke sample :D');
+        $params = $category !== null ? [
+            'limitTo' => [ $category ]
+        ] : [];
+
+        $httpClient = new Client();
+        $httpResponse = $httpClient->get(
+            (new Uri())
+                ->withHost(self::BASE_URL)
+                ->withPath(self::METHOD_RANDOM)
+                ->withQuery(http_build_query($params))
+        );
+        $response = \GuzzleHttp\json_decode($httpResponse->getBody());
+        return $httpResponse->getStatusCode() === 200 ?
+            new Joke($response->value->joke) : new Joke('Ha-ha it\'s failed to get your joke! :D');
     }
 
     public function getCategories() : array
     {
-        // TODO: Implement getCategories() method.
-        return ['explicit', 'nerdy'];
+        $httpClient = new Client();
+        $httpResponse = $httpClient->get(
+            (new Uri())
+                ->withHost(self::BASE_URL)
+                ->withPath(self::METHOD_CATEGORIES)
+        );
+        $response = \GuzzleHttp\json_decode($httpResponse->getBody());
+        return $httpResponse->getStatusCode() === 200 ? $response-> value : [];
     }
 }
